eef1e39c37756adf7238e4badca25b131a55f179
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-03-31 19:39:33 +0100

    1

---
+++ v2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace vowelcounter
+{
+    class vowel
+    {
+        static void Main()
+        {
+            var total = 0;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Type something");
+            
+        }
+    }
+}
