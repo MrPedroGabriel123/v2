779e9c6b5883ef0c71ddad4ac7e57bdeca4f843d
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-03-31 20:25:44 +0100

    ez boy

--- v2/Program.cs
+++ v2/Program.cs
@@ -4,10 +4,20 @@
     {
         static void Main()
         {
-            var total = 0;
-            Console.ForegroundColor = ConsoleColor.Red;
+            int total = 0;
+            var vowel = new List<char> {'a', 'e', 'i', 'o', 'u'}; 
             Console.WriteLine("Type something");
-            
+            var usertext = Console.ReadLine();
+
+            for (int i = 0; i < usertext.Length; i++)
+            {
+                if (vowel.Contains(usertext[i]))
+                {
+                    total++;
+                }
+            }
+            Console.WriteLine("Vowels= {0}",total);
+            Console.ReadLine();
         }
     }
 }
